20da14339e40d770d324ddd851590868c7c65847
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-01-22 12:53:42 -0500

    Update print_helpers.c

--- print_helpers.c
+++ print_helpers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
   * print_number - we're printing some serious numbers
-  * @n: that's the number
+  * @n: that's the number, fear it.
   *
   * Return: void
   */
 int  print_number(unsigned int n)
-{											int retval;
+{
+	int retval;
 	unsigned int num = n;
 
 	if (num / 10)
