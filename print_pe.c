47c55376a9ecc7439116a316def54960b408df59
Newi-M <118599845+Newi-M@users.noreply.github.com>
2023-01-22 23:18:32 +0300

    Create print_pe.c

---
+++ print_pe.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_pe - print %
+ * @args: va_list containing the char to print as the next element
+ *
+ * Return: the number of bytes printed
+ */
+
+int print_pe(va_list args)
+{
+
+	_putchar('%');
+	
+
+	return (1);
+}
