1334e1330d9abce29dc4ca598bfac62ccadf39c6
Newi-M <118599845+Newi-M@users.noreply.github.com>
2023-01-22 17:22:52 +0300

    Create print_d.c

---
+++ print_d.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+  * print_d - prints decimal numbers, positive and negative
+  * @args: the number 
+  *
+  * Return: count
+  */
+int print_d(va_list args)
+{
+	int num = va_arg(args, int);
+  
+	int count = 0;
+	int retval;
+	unsigned int unsign;
+
+	if (num < 0)
+	{
+		retval = _putchar('-');
+		if (retval == -1)
+		{
+			return (-1);
+		}
+		count += 1;
+		unsign = -num;
+
+	}
+	else if (num == 0)
+	{
+		retval = _putchar('0');
+		if (retval == 1)
+			return (1);
+		else
+			return (-1);
+	}
+	else
+	{
+		unsign = num;
+	}
+
+	retval = print_number(unsign);
+
+	if (retval == 1)
+	{
+		count += countDigits(unsign);
+	}
+	else
+		count = -1;
+	return (count);
+}
