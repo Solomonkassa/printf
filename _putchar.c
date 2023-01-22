bb0d8b3ae28cba58ffb4d245f993f6cfbc09e18e
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-01-22 08:26:52 -0500

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
-=======
 
 /**
  * AUTHOR - Solomon kassa && Nawwal Ismael
@@ -14,7 +6,6 @@
  * _putchar - print character
  * @c: character to be printed
  * Return: printed character
->>>>>>> ea892f9e64070e3362b71f495b74ed17f5ee7fab
  */
 int _putchar(char c)
 {
