4ff461d62bde144ca52f39a4a772fc362d73e273
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-01-22 15:52:18 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,59 +1,21 @@
 #include "main.h"
 
 /**
- * _printf - print output to stdout according to a format string
- * @format: the format of the string to print
+ * print_c - print a char argument
+ * @args: va_list containing the char to print as the next element
  *
- * Description: This program emulates some of the functionality of the standard
- * printf function. It does not handle precision, field width, or any of the
- * flags. It does handle all of the format specifiers except "%f" plus some
- * special format specifiers: "%b" prints a number in binary, "%R" encodes a
- * string in ROT13, and "%r" prints a string in reverse. %F is just a little
- * joke that allows you to print an expletive regardless of what you pass the
- * function `print_F`
- *
- * Return: number of characters printed (excluding null byte)
+ * Return: the number of bytes printed
  */
 
-int _printf(const char *format, ...)
+int print_c(va_list args)
 {
-	int count = 0;
-	va_list args;
-	int (*function)(va_list) = NULL;
+	char C = va_arg(args, int);
+	int count = 0, retval;
 
-	va_start(args, format);
+	retval = _putchar(C);
+	if (retval == -1)
+		return (-1);
+	count++;
 
-	while (*format)
-	{
-		if (*format == '%' && *(format + 1) != '%')
-		{
-			format++;
-			function = get_function(format);
-			if (*(format) == '\0')
-				return (-1);
-			else if (function == NULL)
-			{
-				_putchar(*(format - 1));
-				_putchar(*format);
-				count += 2;
-			}
-			else
-				count += function(args);
-		}
-		else if (*format == '%' && *(format + 1) == '%')
-		{
-			format++;
-			_putchar('%');
-			count++;
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
-
-		format++;
-	}
-	va_end(args);
 	return (count);
 }
