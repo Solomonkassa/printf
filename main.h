6672861b56c772e307316727b2d8ab9007298c2d
Newi-M <118599845+Newi-M@users.noreply.github.com>
2023-01-22 18:19:22 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
 /**
  * struct type - connect conversion specifiers with the correct print function
  * @print: a function pointer for the print functions
