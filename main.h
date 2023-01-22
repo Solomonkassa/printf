46ed249f90c4509b1ab95c78e891c929211bb760
solomonkassa <solomonmulu000@gmail.com>
2023-01-22 02:55:36 -0800

    update main.h

--- main.h
+++ main.h
@@ -2,9 +2,23 @@
 #define MAIN_H
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+/**
+ *   Authors &copy;
+ *
+ * - Solomon Kassa  https://github.com/Solomonkassa
+ * - Nawwal Ismael  https://github.com/Newi-M
+ *
+ * struct type - connect conversion specifiers with the correct print function
+ * @print: a function pointer for the print functions
+ * @identifier: the conversion specifier
+ */
+typedef struct type
+{
+	char *hm;
+	int (*print)(va_list);
+} type_t;
 
 int _printf(const char *format, ...);
 
