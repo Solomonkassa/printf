27bd38a50acff20f3ac6750fbd1cb8e7fd3e0447
solomonkassa <solomonmulu000@gmail.com>
2023-01-22 03:20:46 -0800

    man_printf

--- main.h
+++ main.h
@@ -22,5 +22,10 @@ typedef struct type
 } type_t;
 
 int _printf(const char *format, ...);
+int (*get_function(const char *specifier))(va_list);
+int _putchar(char c);
+int _strlen(char *);
+int print_s(va_list args);
+int print_c(va_list args);
 
 #endif
